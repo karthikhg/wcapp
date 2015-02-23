f94572f5aeb9caaf07fb3a5ab15229af6fdd56a1
karthikhg <karthik.hg@gmail.com>
2015-02-22 18:25:07 -0800

    fixed nrr

--- app/models/match.rb
+++ app/models/match.rb
@@ -16,33 +16,39 @@ class Match < ActiveRecord::Base
     return if self.id.present?
     team1 = Team.find self.team1_id
     team2 = Team.find self.team2_id
-byebug
+
     team1.runs_for += self.team1_total
     team2.runs_for += self.team2_total
-    team1.overs_for += self.team1_overs
-    team2.overs_for += self.team2_overs
     team2.runs_against += self.team1_total
     team1.runs_against += self.team2_total
-    team2.overs_against += self.team1_overs
-    team1.overs_against += self.team2_overs
 
     if self.tie
       team1.ties += 1
       team2.ties += 1
       team1.points += 1
       team2.points += 1
+      team1.overs_for += self.team1_overs
+      team2.overs_for += self.team2_overs
+      team2.overs_against += self.team1_overs
+      team1.overs_against += self.team2_overs
     elsif self.team1_total > self.team2_total
       self.winner = team1.id
       team1.wins += 1
       team2.losses += 1
       team1.points += 3
+      team1.overs_for += self.team1_overs
+      team1.overs_against += 50
+      team2.overs_for += 50
+      team2.overs_against += self.team1_overs
     else
       self.winner = team2.id
       team2.wins += 1
       team1.losses += 1
       team2.points += 3
-      # team2.nrr += (self.team2_total/self.team2_overs)-(self.team1_total/self.team1_overs)
-      # team1.nrr += (self.team1_total/self.team1_overs)-(self.team2_total/self.team2_overs)
+      team2.overs_for += self.team2_overs
+      team2.overs_against += 50
+      team1.overs_for += 50
+      team1.overs_against += self.team2_overs
     end
 
     team1.save
